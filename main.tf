4fdebd8bdb331784d3dbe319e3291fbe570fc4cc
nomykhan <52381359+nomykhan@users.noreply.github.com>
2021-07-07 11:19:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "IPspecialist"
 
     workspaces {
       name = "gh-actions-demo"
